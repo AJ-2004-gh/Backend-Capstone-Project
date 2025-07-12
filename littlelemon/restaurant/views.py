35e968093d9a732b1752517ea84724574e3ae0cd
ADWAITH J <jkadwaith1@gmail.com>
2025-07-12 17:12:27 +0530

    API

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
-
-
+from .models import Menu, Booking
+from .serializers import MenuSerializer, BookingSerializer
+from rest_framework.generics import ListAPIView, RetrieveAPIView,DestroyAPIView
+from rest_framework.viewsets import ModelViewSet
 # Create your views here.
 def index(request):
 	return render(request, 'index.html', {})
 # Create your views here.
+class MenuItemView(ListAPIView):
+	queryset = Menu.objects.all()
+	serializer_class = MenuSerializer
+
+class SingleMenuItemView(RetrieveAPIView,DestroyAPIView):
+	queryset = Menu.objects.all()
+	serializer_class = MenuSerializer
+
+class BookingViewSet(ModelViewSet):
+	queryset = Booking.objects.all()
+	serializer_class = BookingSerializer
+
+	
